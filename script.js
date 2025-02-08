6eaeaa1b36906d253858fec74e7ecb495bdb46d4
kanchan <kanchankotkar001@gmail.com>
2025-02-08 21:26:40 +0530

    third commit

--- script.js
+++ script.js
@@ -49,4 +49,69 @@ const updateTodo = (e) => {
         editTodo = e;
     }
 }
+
 // Function to save local todo
+
+    let todos;
+    if (localStorage.getItem("todos") === null) {
+        todos = [];
+    }
+    else {
+        todos = JSON.parse(localStorage.getItem("todos"));
+    }
+    let todoText = todo.children[0].innerHTML;
+    let todoIndex = todos.indexOf(todoText);
+    todos.splice(todoIndex, 1);
+    localStorage.setItem("todos", JSON.stringify(todos));
+    // Array functions : slice / splice
+    console.log(todoIndex);
+    const saveLocalTodos = (todo) => {
+        let todos;
+        if (localStorage.getItem("todos") === null) {
+            todos = [];
+        }
+        else {
+            todos = JSON.parse(localStorage.getItem("todos"));
+        }
+        todos.push(todo);
+        localStorage.setItem("todos", JSON.stringify(todos));
+    }
+    // Function to get local todo
+    const getLocalTodos = () => {
+        let todos;
+        if (localStorage.getItem("todos") === null) {
+            todos = [];
+        }
+        else {
+            todos = JSON.parse(localStorage.getItem("todos"));
+            todos.forEach(todo => {
+                //Creating p tag
+                const li = document.createElement("li");
+                const p = document.createElement("p");
+                p.innerHTML = todo;
+                li.appendChild(p);
+                // Creating Edit Btn
+                const editBtn = document.createElement("button");
+                editBtn.innerText = "Edit";
+                editBtn.classList.add("btn", "editBtn");
+                li.appendChild(editBtn);
+                // Creating Delete Btn
+                const deleteBtn = document.createElement("button");
+                deleteBtn.innerText = "Remove";
+                deleteBtn.classList.add("btn", "deleteBtn");
+                li.appendChild(deleteBtn);
+                todoList.appendChild(li);
+            });
+        }
+    }
+    // Function to delete local todo
+    const deleteLocalTodos = (todo) => {}
+const editLocalTodos = (todo) => {
+    let todos = JSON.parse(localStorage.getItem("todos"));
+    let todoIndex = todos.indexOf(todo);
+    todos[todoIndex] = inputBox.value;
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
+document.addEventListener('DOMContentLoaded', getLocalTodos);
+addBtn.addEventListener('click', addTodo);
+todoList.addEventListener('click', updateTodo);
